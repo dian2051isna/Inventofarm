72f5d4e6f697af3d34933297fca6c6d11bf51b56
Dian Isnariadi <dian.10isnariadi@gmail.com>
2023-11-11 15:17:00 +0700

    Update register.py
    first progress

---
+++ register.py
@@ -0,0 +1,19 @@
+def register() :
+    username = input("Masukkan Username Baru :")
+    password = input("Masukkan Password Baru : ")
+
+    with open('user.csv', 'r') as file :
+        csv_reader = csv.reader(file, delimiter=",")
+        for row in csv_reader :
+            dataa
+def Login() :
+    pass
+
+pilihan = input("Login / Register? (L/R) : ")
+
+if pilihan == R:
+    Register()
+elif pilihan == "L" :
+    Login()
+else :
+    print("Tidak Sesuai")
